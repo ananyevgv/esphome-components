111b9d25493e25146e01be992fc268e72e724360
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2024-01-25 23:54:00 +0300

    Update _cg_Anem.h

--- components/cgAnem/_cg_Anem.h
+++ components/cgAnem/_cg_Anem.h
@@ -32,22 +32,23 @@ class MycgAnem: public PollingComponent, public CustomAPIDevice {
     myself.set_duct_area(ductarea);
     ESP_LOGD("duct_area", "Set to %d", ductarea);
   }
-  void on_resetMinMaxValues(bool reset) {
-    myself.resetMinMaxValues(reset);
-    ESP_LOGD("reset_Min_Max_Value");
+
+  void on_resetMinMaxValues() {
+    myself.resetMinMaxValues();
+    ESP_LOGD("MinMax", "Reset");
   }
+
   void setup() override {
     myself.init();
     myself.set_duct_area(100); //  
     register_service( & MycgAnem::on_set_duct_area, "set_duct_area", {
-      "duct_area"
-    register_service( & MycgAnem::on_resetMinMaxValues, "reset_Min_Max_Values", {
-      "reset_Min_Max"
+      "ductarea"
     });
+    register_service( & MycgAnem::on_resetMinMaxValues, "reset_Min_Max_Values");
   }
 
   void update() override {
-  
+    myself.data_update();
     float temperature = myself.getTemperature();
     float ductArea =  myself.ductArea;
     float airflowRate = myself.getAirflowRate();
