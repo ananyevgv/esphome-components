bd6214b17164fdccf602d0636363a85c2ad6541f
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2024-06-10 15:49:37 +0300

    Update LilygotBattery.cpp

--- components/lilygo_t_battery/LilygotBattery.cpp
+++ components/lilygo_t_battery/LilygotBattery.cpp
@@ -22,22 +22,20 @@ void LilygotBattery::update_battery_info() {
   float vb = analogRead(34);
   float bus = (vb / 4095.0) * 7.26;
   
-  digitalWrite(14, HIGH);
-  delay(10);
-  float v = analogRead(34);
-  //double_t battery_voltage = ((double_t) v / 4095.0) * 2.0 * 3.3 * (this->vref / 1000.0);
-  float battery_voltage = (v / 4095.0) * 7.26;
-  digitalWrite(14, LOW);
-  
   int level = 0;
+  float battery_voltage;
   if (bus > 0) { 
     level = (1-(4.9-bus)/(4.9-4.2))*100;
+    battery_voltage = 0;
   } 
   else {
+    digitalWrite(14, HIGH);
+    delay(10);
+    float v = analogRead(34);
+    battery_voltage = (v / 4095.0) * 7.26;
+    digitalWrite(14, LOW);
     level = (1-(4.1-battery_voltage)/(4.1-3.3))*100;
   }
-
-  
   
   if (level > 100) {level=100;} 
   if (level < 0) {level=0;} 
