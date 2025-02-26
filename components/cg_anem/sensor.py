c893d9d4f2cb5c90cab1fd69e83650453574c322
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2025-02-26 18:03:34 +0300

    Update sensor.py

--- components/cg_anem/sensor.py
+++ components/cg_anem/sensor.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c, sensor
+from esphome.components import i2c, sensor, binary_sensor
 from esphome.const import (
     CONF_ID,
 
