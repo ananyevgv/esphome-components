b683e6e81a19d7c400dfac2dbe8849110ed9187a
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2025-02-27 08:38:58 +0300

    Update cg_anem.h

--- components/cg_anem/cg_anem.h
+++ components/cg_anem/cg_anem.h
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/binary_sensor/binary_sensor.h"
+
 #include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
