74ca8b776d488f6884bf8ae7cae744b9623c18cb
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2025-03-03 12:31:02 +0300

    Update cg_anem.cpp

--- components/cg_anem/cg_anem.cpp
+++ components/cg_anem/cg_anem.cpp
@@ -56,12 +56,6 @@ void CGAnemComponent::setup() {
     this->component_state_ &= ~COMPONENT_STATE_MASK;
     this->component_state_ |= COMPONENT_STATE_CONSTRUCTION;
   }
-  if (!this->read_byte(CG_ANEM_REGISTER_VERSION, &version_raw)) {
-     this->error_code_ = COMMUNICATION_FAILED;
-     this->mark_failed();
-    return;
-  }
-  ESP_LOGI(TAG, "VerRaw: %d", version_raw);
   
   if (!this->read_byte(CG_ANEM_REGISTER_WHO_I_AM, &chip_id)) {
      this->error_code_ = COMMUNICATION_FAILED;
@@ -70,7 +64,8 @@ void CGAnemComponent::setup() {
   }
   ESP_LOGI(TAG, "Id: %d", chip_id);
 
-  
+  this->read_byte(CG_ANEM_REGISTER_VERSION, &this->&version_raw)) {
+  ESP_LOGI(TAG, "VerRaw: %d", version_raw);
 
   
   float version = version_raw / 10.0;
