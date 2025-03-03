7965e807f79fba65311fe93380ecbf14ec1344b5
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2025-03-03 12:43:54 +0300

    Update cg_anem.cpp

--- components/cg_anem/cg_anem.cpp
+++ components/cg_anem/cg_anem.cpp
@@ -107,7 +107,8 @@ void CGAnemComponent::dump_config() {
   LOG_SENSOR("  ", "Max air flow rate", this->max_air_flow_rate_sensor_);
 
 }
-// float CGAnemComponent::get_setup_priority() const { return setup_priority::DATA; }
+ float CGAnemComponent::get_setup_priority() const { return setup_priority::DATA; }
+ void CGAnemComponent::set_duct(uint16_t duct) { this->duct_ = duct; }
 
 void CGAnemComponent::read_status() {
   uint8_t status;
