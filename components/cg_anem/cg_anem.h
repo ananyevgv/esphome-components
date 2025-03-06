085984207ae77f8cfba245f66a55b13e5a06e067
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2025-03-06 12:24:16 +0300

    Update cg_anem.h

--- components/cg_anem/cg_anem.h
+++ components/cg_anem/cg_anem.h
@@ -10,22 +10,25 @@ namespace cg_anem {
 /// This class implements support for the GC Anem Temperature+Pressure+Humidity i2c sensor.
 class CGAnemComponent : public PollingComponent, public i2c::I2CDevice {
  public:
+  void setup() override;
+  void dump_config() override;
+  void update() override;
 
-  void set_ambient_temperature_sensor(sensor::Sensor *ambient_temperature_sensor) { ambient_temperature_sensor_ = ambient_temperature_sensor; }
-  void set_hotend_temperature_sensor(sensor::Sensor *hotend_temperature_sensor) { hotend_temperature_sensor_ = hotend_temperature_sensor; }
-  void set_heat_power_sensor(sensor::Sensor *heat_power_sensor) { heat_power_sensor_ = heat_power_sensor; }
-  void set_air_consumption_sensor(sensor::Sensor *air_consumption_sensor) { air_consumption_sensor_ = air_consumption_sensor; }
-  void set_air_flow_rate_sensor(sensor::Sensor *air_flow_rate_sensor) { air_flow_rate_sensor_ = air_flow_rate_sensor; }
-  void set_firmware_version_sensor(sensor::Sensor *firmware_version_sensor) { firmware_version_sensor_ = firmware_version_sensor; }
-  void set_min_air_flow_rate_sensor(sensor::Sensor *min_air_flow_rate_sensor) { min_air_flow_rate_sensor_ = min_air_flow_rate_sensor; }
-  void set_max_air_flow_rate_sensor(sensor::Sensor *max_air_flow_rate_sensor) { max_air_flow_rate_sensor_ = max_air_flow_rate_sensor; }
+  float get_setup_priority() const override { return setup_priority::DATA; }
+
+  void set_ambient_temperature_sensor(sensor::Sensor *ambient_temperature_sensor) { this->ambient_temperature_sensor_ = ambient_temperature_sensor; }
+  void set_hotend_temperature_sensor(sensor::Sensor *hotend_temperature_sensor) { this->hotend_temperature_sensor_ = hotend_temperature_sensor; }
+  void set_heat_power_sensor(sensor::Sensor *heat_power_sensor) { this->heat_power_sensor_ = heat_power_sensor; }
+  void set_air_consumption_sensor(sensor::Sensor *air_consumption_sensor) { this->air_consumption_sensor_ = air_consumption_sensor; }
+  void set_air_flow_rate_sensor(sensor::Sensor *air_flow_rate_sensor) { this->air_flow_rate_sensor_ = air_flow_rate_sensor; }
+  void set_firmware_version_sensor(sensor::Sensor *firmware_version_sensor) { this->firmware_version_sensor_ = firmware_version_sensor; }
+  void set_min_air_flow_rate_sensor(sensor::Sensor *min_air_flow_rate_sensor) { this->min_air_flow_rate_sensor_ = min_air_flow_rate_sensor; }
+  void set_max_air_flow_rate_sensor(sensor::Sensor *max_air_flow_rate_sensor) { this->max_air_flow_rate_sensor_ = max_air_flow_rate_sensor; }
 
 
   // ========== INTERNAL METHODS ==========
   // (In most use cases you won't need these)
-  void setup() override;
-  void dump_config() override;
-  void update() override;
+
 
   float get_setup_priority() const override;
 
