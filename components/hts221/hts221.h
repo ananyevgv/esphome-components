4e597c34fe7521bcca7bb3641fc4ba33f5bd3991
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2025-05-22 13:34:22 +0300

    Add files via upload

--- components/hts221/hts221.h
+++ components/hts221/hts221.h
@@ -1,27 +1,32 @@
-#include "esphome.h"
-#include "Adafruit_HTS221.h"
+#pragma once
 
-class HTS221CustomSensor : public PollingComponent, public Sensor {
+#include <utility>
+
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
+
+namespace esphome {
+namespace hts221 {
+
+class hts221Component : public PollingComponent, public i2c::I2CDevice {
  public:
-  Adafruit_HTS221 hts;
-  Sensor *temperature_sensor = new Sensor();
-  Sensor *humidity_sensor = new Sensor();
+  void setup() override;
+  void update() override;
+  void dump_config() override;
+  float get_setup_priority() const override;
 
-  HTS221CustomSensor(uint32_t update_interval) : PollingComponent(update_interval) { }
+  void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
+  void set_humidity_sensor(sensor::Sensor *humidity_sensor) { humidity_sensor_ = humidity_sensor; }
 
-  void setup() override {
-    hts.begin_I2C();
-  }
-
-  void update() override {
-    // This is the actual sensor reading logic, which will be called every "update_interval" *milli*seconds
-
-    sensors_event_t temp;
-    sensors_event_t humidity;
-    hts.getEvent(&humidity, &temp);// populate temp and humidity objects with fresh data
-
-    temperature_sensor->publish_state(temp.temperature);
-    humidity_sensor->publish_state(humidity.relative_humidity);
-
-  }
+ protected:
+  sensor::Sensor *temperature_sensor_{nullptr};
+  sensor::Sensor *humidity_sensor_{nullptr};
+  unsigned read_count_{};
+  void read_data_();
+  void restart_read_();
+  uint32_t start_time_{};
 };
+
+}  // namespace hts221
+}  // namespace esphome
