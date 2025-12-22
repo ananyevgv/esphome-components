9546ca7e96309c2add8737617ba10332655631eb
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2025-12-22 11:47:40 +0300

    Update sensor.py

--- components/lilygo_t_battery/sensor.py
+++ components/lilygo_t_battery/sensor.py
@@ -5,6 +5,8 @@ from esphome import pins
 from esphome.const import (
     CONF_ID,
     CONF_PIN,
+    CONF_SENSOR,
+    CONF_REFERENCE_VOLTAGE,
     CONF_ENABLE_PIN,
     CONF_VOLTAGE,
     CONF_BUS_VOLTAGE,
@@ -28,6 +30,8 @@ CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(): cv.declare_id(Lilygotbattery),
         cv.Optional(CONF_ENABLE_PIN): pins.gpio_output_pin_schema,
+        cv.Optional(CONF_REFERENCE_VOLTAGE, default="3.3V"): cv.voltage,
+        cv.Required(CONF_SENSOR): cv.use_id(sensor.Sensor),
         cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(
             unit_of_measurement=UNIT_VOLT,
             icon=ICON_BATTERY,
@@ -57,7 +61,10 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
+    
+    sens = await cg.get_variable(config[CONF_SENSOR])
+    cg.add(var.set_sensor(sens))
+    
     if CONF_VOLTAGE in config:
       sens = await sensor.new_sensor(config[CONF_VOLTAGE])
       cg.add(var.set_voltage_sensor(sens))
