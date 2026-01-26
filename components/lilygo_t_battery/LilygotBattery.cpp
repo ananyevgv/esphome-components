7f17f9ad71e7d2110d5962a8f94cb51ba788cc19
ananyevgv <81970899+ananyevgv@users.noreply.github.com>
2026-01-26 16:02:59 +0300

    Add files via upload

--- components/lilygo_t_battery/LilygotBattery.cpp
+++ components/lilygo_t_battery/LilygotBattery.cpp
@@ -6,12 +6,6 @@ namespace lilygo_t_battery {
 
 static const char *const TAG = "lilygo";
 
-void LilygotBattery::setup() {
-  esp_adc_cal_characteristics_t adc_chars;
-  esp_adc_cal_value_t val_type = esp_adc_cal_characterize((adc_unit_t)ADC_UNIT_1, (adc_atten_t)ADC_ATTEN_DB_2_5, (adc_bits_width_t)ADC_WIDTH_BIT_12, 1100, &adc_chars);
-  pinMode(14, OUTPUT);
-
-}
 void LilygotBattery::dump_config() {
   ESP_LOGCONFIG(TAG, "lilygo:");
   LOG_SENSOR("  ", "USB voltage", this->voltage);
@@ -21,7 +15,7 @@ void LilygotBattery::dump_config() {
 }
 
 void LilygotBattery::update() {
-  
+
   // wait for voltage to stabilise
   delay(1);
   LilygotBattery::update_battery_info();
@@ -29,9 +23,10 @@ void LilygotBattery::update() {
 void LilygotBattery::update_battery_info() {
   LilygotBattery::correct_adc_reference();
 
-  float vb = analogRead(34);
+  float vb = this->reference_voltage_;
+  // analogRead(34);
   float bus = (vb / 4095.0) * 7.26;
-  
+
   int level = 0;
   float battery_voltage;
   if (bus > 0) { 
@@ -39,21 +34,30 @@ void LilygotBattery::update_battery_info() {
     battery_voltage = 0;
   } 
   else {
-    digitalWrite(14, HIGH);
+    if (this->enable_pin_ != nullptr) {
+      this->enable_pin_->setup();
+      this->enable_pin_->digital_write(true);
+    }    
+
     delay(10);
-    float v = analogRead(34);
+    float v = this->reference_voltage_;
+    // analogRead(34);
     battery_voltage = (v / 4095.0) * 7.26;
-    digitalWrite(14, LOW);
+    if (this->enable_pin_ != nullptr) {
+      this->enable_pin_->setup();
+      this->enable_pin_->digital_write(false);
+    }
+
     level = (1-(4-battery_voltage)/(4-3.25))*100;
   }
-  
+
   if (level > 100) {level=100;} 
   if (level < 0) {level=0;} 
-  
+
   ESP_LOGD(TAG, "USB voltage: %0.2f V", bus);
   ESP_LOGD(TAG, "Battery charge: %d %%", level);
   ESP_LOGD(TAG, "Battery voltage: %0.2f V", battery_voltage);
-  
+
   if (this-> bus_voltage != nullptr) {
     this->bus_voltage->publish_state(bus);
   }
@@ -62,19 +66,10 @@ void LilygotBattery::update_battery_info() {
   }
   if (this->voltage != nullptr) {
     this->voltage->publish_state(battery_voltage);
-    
+
   }
 }
 
-void LilygotBattery::correct_adc_reference() {
-  esp_adc_cal_characteristics_t adc_chars;
-  esp_adc_cal_value_t val_type =
-      esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_12, 1100, &adc_chars);
-  if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
-    this->vref = adc_chars.vref;
-  }
-}
 
 }  // namespace lilygo_t_battery
 }  // namespace esphome
-
--- components/lilygo_t_battery/LilygotBattery.h
+++ components/lilygo_t_battery/LilygotBattery.h
@@ -11,22 +11,35 @@
 namespace esphome {
 namespace lilygo_t_battery {
 
-class LilygotBattery : public PollingComponent {
+class LilygotBattery : public Component, public sensor::Sensor  {
  public:
   sensor::Sensor *voltage{nullptr};
   sensor::Sensor *bus_voltage{nullptr};
   sensor::Sensor *battery_level{nullptr};
-  
+
   int vref = 1100;
-  void setup() override;
-  void update() override;
+  void setup() override {
+    this->sensor_->add_on_state_callback([this](float value) { this->process_(value); });
+    if (this->sensor_->has_state())
+      this->process_(this->sensor_->state);
+  }
+
   void update_battery_info();
   void correct_adc_reference();
   void dump_config() override;
 
+  void set_sensor(Sensor *sensor) { sensor_ = sensor; }
+  void set_enable_pin(GPIOPin *enable) { this->enable_pin_ = enable; }
+  void set_reference_voltage(float reference_voltage) { reference_voltage_ = reference_voltage; }
   void set_voltage_sensor(sensor::Sensor *voltage_sensor) { voltage = voltage_sensor; }
   void set_bus_voltage_sensor(sensor::Sensor *bus_voltage_sensor) { bus_voltage = bus_voltage_sensor; }
   void set_battery_level_sensor(sensor::Sensor *battery_level_sensor) { battery_level = battery_level_sensor; }
+
+ protected:
+  sensor::Sensor *sensor_;
+  GPIOPin *enable_pin_{nullptr};
+  float reference_voltage_;
+  void process_(float value);
+
 };
 }  // namespace lilygo_t_battery
-}  // namespace esphome
